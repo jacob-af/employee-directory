f0e092ca6543ee93e938cbb8abc93bacdc380173
jacob-af <jfeitler@gmail.com>
2021-04-20 23:40:15 -0400

    finalized filter categories

--- src/components/SortList.js
+++ src/components/SortList.js
@@ -2,8 +2,12 @@ const SortList = props => {
   return (
     <select onChange={e => props.sort(e.target.value)}>
       <option value="none">Sort List</option>
-      <option value="first">First Name</option>
-      <option value="last">Last Name</option>
+      <option value="first">First Name (ASC)</option>
+      <option value="first-d">First Name (DESC)</option>
+      <option value="last">Last Name (ASC)</option>
+      <option value="last-d">Last Name (DESC)</option>
+      <option value="age">Age (ASC)</option>
+      <option value="age-d">Age (DESC)</option>
     </select>
   );
 };
