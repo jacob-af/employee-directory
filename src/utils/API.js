ac617b1e99a01f7d73ba3e30a9b89b8f7b05ad53
jacob-af <jfeitler@gmail.com>
2021-04-21 03:29:40 -0400

    increased  records to api request

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASEURL = "https://randomuser.me/api/?results=100&nat=us,gb,dk,de,fr";
+const BASEURL = "https://randomuser.me/api/?results=500&nat=us,gb,dk,de,fr";
 
 const API = {
   search: function () {
