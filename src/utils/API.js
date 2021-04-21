56b884d354b3a7524d496a155d0e7abc8c7974ee
jacob-af <jfeitler@gmail.com>
2021-04-20 23:41:57 -0400

    added arguments to api search for better data

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASEURL = "https://randomuser.me/api/?results=50";
+const BASEURL = "https://randomuser.me/api/?results=100&nat=us,gb,dk,de,fr";
 
 const API = {
   search: function () {
